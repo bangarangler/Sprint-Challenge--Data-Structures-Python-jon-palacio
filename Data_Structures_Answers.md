93b8dd18f3a0418b196df67fc7d30068ec33749b
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-06-14 13:56:09 -0400

    Completed Sprint

--- Data_Structures_Answers.md
+++ Data_Structures_Answers.md
@@ -1,18 +1,27 @@
 Add your answers to the questions below.
 
 1. What is the runtime complexity of your ring buffer's `append` method?
+O(1) or constant
 
 2. What is the space complexity of your ring buffer's `append` function?
+O(1) or constant
 
 3. What is the runtime complexity of your ring buffer's `get` method?
+O(n) or linear
 
 4. What is the space complexity of your ring buffer's `get` method?
+O(n) or linear
 
 
 5. What is the runtime complexity of the provided code in `names.py`?
+O(n^2) or quadratic
 
 6. What is the space complexity of the provided code in `names.py`?
+O(n) or linear
 
 7. What is the runtime complexity of your optimized code in `names.py`?
+O(n) or linear MVP
+O(n*log(n))
 
 8. What is the space complexity of your optimized code in `names.py`?
+O(n) or linear. && o(n) or linear
--- names/names.py
+++ names/names.py
@@ -11,10 +11,32 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1: # runtime: 5.228913....
+    # for name_2 in names_2:
+        # if name_1 == name_2:
+            # duplicates.append(name_1)
+
+names = names_1 + names_2
+'''
+This way we don't repeat names like in the original
+O(n)
+'''
+unique = set()
+for name in names: # runtime: 0.00717687...
+    if name in unique:
+        duplicates.append(name)
+    else:
+        unique.add(name)
+
+'''
+This can handle repeated names if there are any.
+none in this case so O(n*log(n))
+'''
+
+# names.sort()
+# for i in range(len(names) - 1): # runtime: 0.01009607...
+    # if names[i] == names[i + 1]:
+        # duplicates.append(names[i])
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
